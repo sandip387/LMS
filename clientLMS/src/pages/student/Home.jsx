2ce6f7ac0af48f42fea1329f205e7efed8947b23
Sandip Shrestha <sandipshrestha387@gmail.com>
2025-02-19 19:03:00 +0545

    code Hero component with font size extended in tailwind.config.js file

--- clientLMS/src/pages/student/Home.jsx
+++ clientLMS/src/pages/student/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import Hero from "../../components/student/Hero.jsx";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="flex flex-col items-center space-y-7 text-center">
+      <Hero />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
