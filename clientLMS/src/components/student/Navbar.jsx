beff3a4a7db18b78353322a29b0d8275ad5e6430
Sandip Shrestha <sandipshrestha387@gmail.com>
2025-02-19 18:18:45 +0545

    provided Clerk Authentication

--- clientLMS/src/components/student/Navbar.jsx
+++ clientLMS/src/components/student/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const isCourseListPage = location.pathname.includes("/course-list");
+
+  const {openSignIn} = useClerk();
+  const {user} = useUser();
 
   return (
     <div
@@ -17,15 +21,24 @@ const Navbar = () => {
         className="w-28 lg:w-32 cursor-pointer"
       />
       <div className="hidden md:flex items-center gap-5 text-gray-500 ">
-        <div>
+        <div className="flex items-center gap-5">
           <button>Become Educator</button>
-          <Link to="/my-enrollments">My Emrollments</Link>
+          <Link to="/my-enrollments">My Enrollments</Link>
         </div>
-        <button className="bg-blue-600 text-white px-5 py-2 rounded-full">
+        <button onClick={()=> openSignIn()} className="bg-blue-600 text-white px-5 py-2 rounded-full">
           Create Account
         </button>
       </div>
-      <div></div>
+      {/* {For Phone Screens} */}
+      <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
+        <div>
+          <button>Become Educator</button>
+          <Link to="/my-enrollments">My Enrollments</Link>
+        </div>
+        <button>
+          <img src={assets.user_icon} alt="" width="30px" height="auto" />
+        </button>
+      </div>
     </div>
   );
 };
