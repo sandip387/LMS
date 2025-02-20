6409618e3b0731d35c898ff49dd13961d4d384f7
Sandip Shrestha <sandipshrestha387@gmail.com>
2025-02-20 22:12:09 +0545

    added CoursesSection with courseCard UI

--- clientLMS/src/context/AppContext.jsx
+++ clientLMS/src/context/AppContext.jsx
@@ -1,9 +1,26 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
-  const value = {};
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const [allCourses, setAllCourses] = useState([]);
+
+  //fetch all courses
+  const fetchAllCourse = async () => {
+    setAllCourses(dummyCourses);
+  };
+
+  useEffect(() => {
+    fetchAllCourse();
+  }, []);
+
+  const value = {
+    currency,
+    allCourses,
+  };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
