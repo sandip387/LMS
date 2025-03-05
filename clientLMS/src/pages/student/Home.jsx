2534194f4b8566e1e5663e3ae3b10ce18250a3b3
Sandip Shrestha <sandipshrestha387@gmail.com>
2025-03-05 22:33:51 +0545

    Testimonial section

--- clientLMS/src/pages/student/Home.jsx
+++ clientLMS/src/pages/student/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "../../components/student/Hero.jsx";
 import Companies from "../../components/student/Companies.jsx";
 import CoursesSection from "../../components/student/CoursesSection.jsx";
+import TestimonialsSection from "../../components/student/TestimonialsSection.jsx";
 const Home = () => {
   return (
     <div className="flex flex-col items-center space-y-7 text-center">
       <Hero />
       <Companies />
       <CoursesSection />
+      <TestimonialsSection />
     </div>
   );
 };
