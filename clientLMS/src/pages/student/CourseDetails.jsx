411dd218bac244d85dcbf6923ce419e71ac25393
Sandip Shrestha <sandipshrestha387@gmail.com>
2025-03-06 22:34:37 +0545

    make course detail visible

--- clientLMS/src/pages/student/CourseDetails.jsx
+++ clientLMS/src/pages/student/CourseDetails.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
+import Loading from "./../../components/student/Loading";
 
 const CourseDetails = () => {
-  return (
-    <div>CourseDetails</div>
-  )
-}
+  const { id } = useParams();
+  const [courseData, setCourseData] = useState(null);
+  const { allCourses } = useContext(AppContext);
+  const fetchCourseData = async () => {
+    const findCourse = allCourses.find((course) => course._id === id);
+    setCourseData(findCourse);
+  };
 
-export default CourseDetails
+  useEffect(() => {
+    fetchCourseData();
+  }, []);
+  return courseData ? (
+    <>
+      <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
+        <div className="absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70"></div>
+        {/*left column*/}
+        <div>
+          <h1>{courseData.courseTitle}</h1>
+          <p>{courseData.courseDescription}</p>
+        </div>
+      </div>
+    </>
+  ) : (
+    <Loading />
+  );
+};
+
+export default CourseDetails;
