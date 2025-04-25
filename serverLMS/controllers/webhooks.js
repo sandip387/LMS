266660bd00adedb96bc3647a0c0933cffbd229ea
Sandip Shrestha <sandipshrestha387@gmail.com>
2025-04-25 20:45:53 +0545

    webhook amendment

--- serverLMS/controllers/webhooks.js
+++ serverLMS/controllers/webhooks.js
@@ -6,13 +6,13 @@ import User from "../models/User.js";
 export const clerkWebhooks = async (req, res) => {
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-        await whook.verify(JSON.stringify(req.body), {
+        await whook.verify(payloadString, {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
         })
 
-        const { data, type } = req.body;
+        const { data, type } = JSON.parse(payloadString);
 
         switch (type) {
             case 'user.created': {
