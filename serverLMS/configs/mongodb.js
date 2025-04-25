9d34ddd9f7f6a2edd00fd78da64b9f045070c6fc
Sandip Shrestha <sandipshrestha387@gmail.com>
2025-04-25 19:56:20 +0545

    user  webhooks corrected

--- serverLMS/configs/mongodb.js
+++ serverLMS/configs/mongodb.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    mongoose.connection.on('connected', () => console.log('Database Connected'));
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/e-shikshya`);
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/e-shikshya`);
+        console.log(`\n MongoDB connected !! DB HOST:${connectionInstance.connection.host} `);
+    } catch (error) {
+        console.log("MONGODB connection FAILED", error);
+        process.exit(1);
+    }
 }
 
 export default connectDB;
