4beb7b4e0c7797257adf09d13baf4a33f84d45ff
Sandip Shrestha <sandipshrestha387@gmail.com>
2025-04-25 20:11:24 +0545

    raw bodyparser for clerk

--- serverLMS/server.js
+++ serverLMS/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/mongodb.js';
 import { clerkWebhooks } from './controllers/webhooks.js';
+import bodyParser from 'body-parser';
 
 
 const app = express();
@@ -12,14 +13,22 @@ await connectDB();
 //Middlewares
 app.use(cors());
 
+
+
+// We add express.json() only AFTER the /clerk route
+app.post(
+    '/clerk',
+    bodyParser.raw({ type: 'application/json' }),  // <-- This gives raw body
+    clerkWebhooks
+);
+
+// General middleware
+app.use(express.json());
+
 //Routes
 app.get('/', (req, res) => {
     res.send("API Working");
 })
-
-app.use(express.json()); // globally parse JSON
-app.post('/clerk', clerkWebhooks);
-
 
 //Port
 const PORT = process.env.PORT || 5000;
