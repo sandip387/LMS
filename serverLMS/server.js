f2392a7c54dc71ac0d8581949a6a4d4b5301a4d9
Sandip Shrestha <sandipshrestha387@gmail.com>
2025-05-14 18:24:40 +0545

    APIs for courses

--- serverLMS/server.js
+++ serverLMS/server.js
@@ -6,6 +6,7 @@ import { clerkWebhooks } from './controllers/webhooks.js';
 import educatorRouter from './routes/educatorRoutes.js';
 import { clerkMiddleware } from '@clerk/express';
 import connectCloudinary from './configs/cloudinary.js';
+import courseRouter from './routes/courseRoute.js';
 
 
 const app = express();
@@ -34,6 +35,7 @@ app.post(
 );
 
 app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/course', express.json(), courseRouter);
 
 //Port
 const PORT = process.env.PORT || 5000;
