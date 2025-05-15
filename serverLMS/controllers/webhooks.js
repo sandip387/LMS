c90cd60280e30f006287e42b946140f72e7fddea
Sandip Shrestha <sandipshrestha387@gmail.com>
2025-05-15 17:45:43 +0545

    corrected stripe webhook function

--- serverLMS/controllers/webhooks.js
+++ serverLMS/controllers/webhooks.js
@@ -62,7 +62,7 @@ export const clerkWebhooks = async (req, res) => {
 
 const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
 
-export const stripeWebhooks = async (req, res) => {
+export const stripeWebhooks = async (request, response) => {
     const sig = request.headers['stripe-signature'];
 
     let event;
